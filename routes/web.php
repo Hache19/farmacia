d9447c9336874aaf5a75aa6ccfea812ab3a5d073
Hache19 <michel.hmro19@gmail.com>
2021-11-25 03:30:58 -0400

    jetstream

--- routes/web.php
+++ routes/web.php
@@ -26,22 +26,28 @@ use App\Http\Controllers\LoteController;
 */
 
 Route::get('/', HomeController::class);
+
+//Rol
 Route::get('rol',[RolController::class, 'index']);
 Route::get('rol/create',[RolController::class, 'create']);
 Route::get('rol/{rol}',[RolController::class, 'show']);
 
+//Personal
 Route::get('personal',[PersonalController::class, 'index']);
 Route::get('personal/create',[PersonalController::class, 'create']);
 Route::get('personal/{personal}',[PersonalController::class, 'show']);
 
+//Presentacion
 Route::get('presentacion',[PresentacionController::class, 'index']);
 Route::get('presentacion/create',[PresentacionController::class, 'create']);
 Route::get('presentacion/{presentacion}',[PresentacionController::class, 'show']);
 
+//tipoproducto
 Route::get('tipoproducto',[TipoProductoController::class, 'index']);
 Route::get('tipoproducto/create',[TipoProductoController::class, 'create']);
 Route::get('tipoproducto/{tipoproducto}',[TipoProductoController::class, 'show']);
 
+//laboratorio
 Route::get('laboratorio',[LaboratorioController::class, 'index']);
 Route::get('laboratorio/create',[LaboratorioController::class, 'create']);
 Route::get('laboratorio/{laboratorio}',[LaboratorioController::class, 'show']);
@@ -56,19 +62,25 @@ Route::get('proveedor', [ProveedorController::class,'index'] );
 Route::get('proveedor/create', [ProveedorController::class,'create']);
 Route::get('proveedor/{proveedor}', [ProveedorController::class,'show']);
 
-//proveedor
+//venta
 Route::get('venta', [VentaController::class,'index'] );
 Route::get('venta/create', [VentaController::class,'create']);
 Route::get('venta/{venta}', [VentaController::class,'show']);
 
+//producto
 Route::get('producto', [ProductoController::class,'index'] );
 Route::get('producto/create', [ProductoController::class,'create']);
 Route::get('producto/{producto}', [ProductoController::class,'show']);
 
+//productoventa
 Route::get('productoventa', [ProductoVentaController::class,'index'] );
 Route::get('productoventa/create', [ProductoVentaController::class,'create']);
 Route::get('productoventa/{productoventa}', [ProductoVentaController::class,'show']);
 
+//Lote
 Route::get('lote', [LoteController::class,'index'] );
 Route::get('lote/create', [LoteController::class,'create']);
 Route::get('lote/{lote}', [LoteController::class,'show']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
